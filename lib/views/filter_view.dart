fcd3f0366ab4f531acd119016d3bdde40a769631
Jaysharma8811 <jaysharma8811@gmail.com>
2024-08-12 14:50:06 +0530

    adding filter screen

---
+++ lib/views/filter_view.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+
+enum Filter{
+  glutenFree,
+  lactoseFree,
+  vegetarian,
+  vegan
+}
+
+class FilterScreen extends StatefulWidget {
+  const FilterScreen({super.key, required this.currentFilters});
+
+  final Map<Filter,bool> currentFilters;
+
+  @override
+  State<FilterScreen> createState() {
+    return _FilterScreenState();
+  }
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  var _glutenFreeFilterSet = false;
+  var _lactoseFreeFilterSet = false;
+  var _veganFilterSet = false;
+  var _vegetarianFilterSet = false;
+  @override
+  void initState() {
+    super.initState();
+    _glutenFreeFilterSet=widget.currentFilters[Filter.glutenFree]!;
+    _lactoseFreeFilterSet=widget.currentFilters[Filter.lactoseFree]!;
+    _vegetarianFilterSet=widget.currentFilters[Filter.vegetarian]!;
+    _veganFilterSet=widget.currentFilters[Filter.vegan]!;
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(' your Filters'),
+      ),
+      // drawer: MainDrawer(
+      //   onSelectScreen: (identifier) {
+      //     Navigator.of(context).pop();
+      //     if (identifier == 'meals') {
+      //       Navigator.of(context).pushReplacement(
+      //         MaterialPageRoute(
+      //           builder: (ctx) => const TapScreen(),
+      //         ),
+      //       );
+      //     }
+      //   },
+      // ),
+      body: WillPopScope(
+        onWillPop: () async{ Navigator.of(context).pop({
+          Filter.glutenFree:_glutenFreeFilterSet,
+          Filter.lactoseFree:_lactoseFreeFilterSet,
+          Filter.vegetarian:_vegetarianFilterSet,
+          Filter.vegan:_veganFilterSet
+        });
+          return false;
+          },
+        child: Column(
+          children: [
+            SwitchListTile(
+              value: _glutenFreeFilterSet,
+              onChanged: (newValue) {
+                setState(() {
+                  _glutenFreeFilterSet = newValue;
+                });
+              },
+              title: Text(
+                'Gluten-Free',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: Theme.of(context).colorScheme.onBackground),
+              ),
+              subtitle: Text(
+                'Only include gluten_free meals',
+                style: Theme.of(context)
+                    .textTheme
+                    .labelMedium!
+                    .copyWith(color: Theme.of(context).colorScheme.onBackground),
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(left: 34, right: 22),
+            ),
+            SwitchListTile(
+              value: _lactoseFreeFilterSet,
+              onChanged: (newValue) {
+                setState(() {
+                  _lactoseFreeFilterSet = newValue;
+                });
+              },
+              title: Text(
+                'Lactose-Free',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: Theme.of(context).colorScheme.onBackground),
+              ),
+              subtitle: Text(
+                'Only include Lactose_free meals',
+                style: Theme.of(context)
+                    .textTheme
+                    .labelMedium!
+                    .copyWith(color: Theme.of(context).colorScheme.onBackground),
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(left: 34, right: 22),
+            ),
+            SwitchListTile(
+              value: _vegetarianFilterSet,
+              onChanged: (newValue) {
+                setState(() {
+                  _vegetarianFilterSet = newValue;
+                });
+              },
+              title: Text(
+                'Vegetarian Meal',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: Theme.of(context).colorScheme.onBackground),
+              ),
+              subtitle: Text(
+                'Only include vegetarian meals',
+                style: Theme.of(context)
+                    .textTheme
+                    .labelMedium!
+                    .copyWith(color: Theme.of(context).colorScheme.onBackground),
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(left: 34, right: 22),
+            ),
+            SwitchListTile(
+              value: _veganFilterSet,
+              onChanged: (newValue) {
+                setState(() {
+                  _veganFilterSet = newValue;
+                });
+              },
+              title: Text(
+                'Vegan Meal ',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: Theme.of(context).colorScheme.onBackground),
+              ),
+              subtitle: Text(
+                'Only include Vegan meals',
+                style: Theme.of(context)
+                    .textTheme
+                    .labelMedium!
+                    .copyWith(color: Theme.of(context).colorScheme.onBackground),
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(left: 34, right: 22),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/views/tap_view.dart
+++ lib/views/tap_view.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:foodmeal/data/avilable_categories.dart';
 import 'package:foodmeal/models/meals_model.dart';
 import 'package:foodmeal/views/categories_view.dart';
+import 'package:foodmeal/views/filter_view.dart';
 import 'package:foodmeal/views/meals_view.dart';
+import 'package:foodmeal/widgets/main_drawer.dart';
+
+const kInitialFilters={
+  Filter.glutenFree:false,
+  Filter.lactoseFree:false,
+  Filter.vegetarian:false,
+  Filter.vegan:false
+};
 
 class TapScreen extends StatefulWidget {
   const TapScreen({super.key});
@@ -13,58 +23,100 @@ class TapScreen extends StatefulWidget {
 }
 
 class _TapScreenState extends State<TapScreen> {
+  final List<Meal> _favouriteMeals = [];
+  Map<Filter,bool> _selectedFilters=kInitialFilters;
 
-  final List<Meal> _favouriteMeals=[];
-  void _showInfo(String message){
+  void _showInfo(String message) {
     ScaffoldMessenger.of(context).clearSnackBars();
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
   }
-  void _toggleMealsFavouriteStatus(Meal meal){
-    final isExisting=_favouriteMeals.contains(meal);
-    if(isExisting){
+
+  void _toggleMealsFavouriteStatus(Meal meal) {
+    final isExisting = _favouriteMeals.contains(meal);
+    if (isExisting) {
       setState(() {
         _favouriteMeals.remove(meal);
         _showInfo("Meal is no longer favourite!");
-
       });
-
-    }else{
+    } else {
       setState(() {
         _favouriteMeals.add(meal);
         _showInfo("meal marked as a favourite");
-
       });
     }
   }
-  int _selectedPageindex=0;
-  void _selectPage(int index){
+
+  int _selectedPageindex = 0;
+  void _selectPage(int index) {
     setState(() {
-      _selectedPageindex=index;
+      _selectedPageindex = index;
     });
   }
+
+  void _setScreen(String identifier) async {
+    Navigator.of(context).pop();
+    if (identifier == 'filters') {
+
+     final result = await Navigator.of(context).push<Map<Filter,bool>>(
+        MaterialPageRoute(
+          builder: (ctx) =>  FilterScreen(currentFilters: _selectedFilters,),
+        ),
+      );
+     setState(() {
+       _selectedFilters=result??kInitialFilters;
+
+     });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    Widget activePage=  CategoriesScreen(onToggleFavourite: _toggleMealsFavouriteStatus,);
-    var activePageTitle="Categories";
-    if(_selectedPageindex==1){
-       activePage= MealsScreen(title: "Favourites", meals: _favouriteMeals,onToggleFavourite: _toggleMealsFavouriteStatus,);
-       activePageTitle="Your favourites";
 
+    final availableMeals=dummyMeals.where((meal) {
+      if(_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree){
+        return false;
+      }
+      if(_selectedFilters[Filter.lactoseFree]! && !meal.isLactoseFree){
+        return false;
+      }
+      if(_selectedFilters[Filter.vegetarian]! && !meal.isVegetarian){
+        return false;
+      }
+      if(_selectedFilters[Filter.vegan]! && !meal.isVegan){
+        return false;
+      }
+      return true;
+    }).toList();
+    Widget activePage = CategoriesScreen(
+      availableMeals: availableMeals,
+      onToggleFavourite: _toggleMealsFavouriteStatus,
+    );
+    var activePageTitle = "Categories";
+    if (_selectedPageindex == 1) {
+      activePage = MealsScreen(
+        meals: _favouriteMeals,
+        onToggleFavourite: _toggleMealsFavouriteStatus,
+      );
+      activePageTitle = "Your favourites";
     }
     return Scaffold(
       appBar: AppBar(
-        title:  Text(activePageTitle),
+        title: Text(activePageTitle),
+      ),
+      drawer: MainDrawer(
+        onSelectScreen: _setScreen,
       ),
       body: activePage,
-      bottomNavigationBar:  BottomNavigationBar(
+      bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedPageindex,
-        onTap: (index){
+        onTap: (index) {
           _selectPage(index);
         },
         items: const [
-          BottomNavigationBarItem(icon:Icon(Icons.set_meal) , label:'Categories' ),
-          BottomNavigationBarItem(icon:Icon(Icons.star) , label:'Favorites' ),
-
+          BottomNavigationBarItem(
+              icon: Icon(Icons.set_meal), label: 'Categories'),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favorites'),
         ],
       ),
     );
